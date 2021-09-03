069ca07c879f0adebda8679ba1fb4e8d696db1a4
nozibuddowla <nozibuddowla@gmail.com>
2021-09-03 12:42:51 +0600

    add fetch url

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ const API_KEY = `c31a3bd42e0373fb4da8b0ea202fc0a7`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name');
     const cityName = city.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayTemperature(data))
+}
+
+const displayTemperature = temperature => {
+    console.log(temperature);
 }
